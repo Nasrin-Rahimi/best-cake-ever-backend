1ccaf152da1417d368628287cb6a3b60be2a1861
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-21 10:54:40 -0400

    add api and v1 namespacing to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  resources :orderdetails
-  resources :orders
-  resources :products
-  resources :categories
-  resources :customers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      
+      resources :customers
+
+      resources :orders do
+        resources :orderdetails
+      end
+     
+      resources :categories do
+        resources :products
+      end
+      
+    end
+  end
 end
