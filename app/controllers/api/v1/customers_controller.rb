6f26cc65c885967c5132de98decacdb9896db99b
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-21 11:21:39 -0400

    add show action for customer_controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::CustomersController < ApplicationController
+
+    def show
+        customer = Customer.find_by(id: params[:id])
+        if(customer)
+            render json: {id: customer.id, name: customer.name, orders: customer.orders}
+        else
+            render json: {message: 'Customer Not Found'}
+        end
+    end
+
 end
