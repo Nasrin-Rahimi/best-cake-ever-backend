6b4f9fb1e83b52427e570d9e8830156ff5b0b5db
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-21 06:31:20 -0400

    generate product resource

---
+++ db/migrate/20210421103048_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.float :unit_price
+      t.string :weight
+      t.string :description
+      t.string :image
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
