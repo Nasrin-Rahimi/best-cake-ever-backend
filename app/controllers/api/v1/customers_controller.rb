764c51984f7017798cab19c176c4a338af16c6e5
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-24 05:47:42 -0400

    add serializer for customer

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -3,7 +3,9 @@ class Api::V1::CustomersController < ApplicationController
     def show
         customer = Customer.find_by(id: params[:id])
         if(customer)
-            render json: {id: customer.id, name: customer.name, orders: customer.orders}
+            # render json: {id: customer.id, name: customer.name, orders: customer.orders}
+            customer_json = CustomerSerializer.new(customer).serialized_json
+            render json: customer_json
         else
             render json: {message: 'Customer Not Found'}
         end
