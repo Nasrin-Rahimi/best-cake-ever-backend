4f16e1ee835c2b94b73a9f488bfe041346523554
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-25 12:32:42 -0400

    create customer

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::SessionsController < ApplicationController
+
+    def create
+       customer = Customer.find_by(name: params[:session][:name])
+
+        if customer && customer.authenticate(params[:session][:password])
+            session[:customer_id] = customer.id
+            render json: customer
+        else
+            render json: {
+                error: "Invalid Credentials"
+            }
+        end
+    end
+    
 end
