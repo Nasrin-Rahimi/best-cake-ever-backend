d906e0971300d6d7a2a1d52ca0d2f878e0d8b39a
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-26 04:31:26 -0400

    helper method for getting current customer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+
+    def current_customer
+        Customer.find(session[:customer_id])
+    end
+
+    def logged_in?
+        !!current_customer
+    end
+    
 end
