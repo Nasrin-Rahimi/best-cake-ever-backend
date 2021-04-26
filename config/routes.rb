eb3326464f6f199354ae65f34d685320fd7e07c0
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-26 05:04:47 -0400

    add route for get current customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post "api/v1/login", to: "api/v1/sessions#create"
+  get "api/v1/get_current_customer", to: "api/v1/sessions#get_current_customer"
 
   namespace :api do
     namespace :v1 do
