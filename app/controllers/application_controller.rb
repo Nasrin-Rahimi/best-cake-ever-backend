22ace166ac90e38ab764be5029db4df104b920d3
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-26 05:43:59 -0400

    currect current_customer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::API
 
     def current_customer
-        Customer.find(session[:customer_id])
+        Customer.find_by(id: session[:customer_id])
     end
 
     def logged_in?
         !!current_customer
     end
-    
+
 end
