0ad1ea9ef507a7e8a8fd59134308b4f9eeb57fc1
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-26 05:09:27 -0400

    get_current_customer action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -12,5 +12,18 @@ class Api::V1::SessionsController < ApplicationController
             }
         end
     end
+
+    # get_current_customer route check if we're logged in, which checks the current_customer
+    # and session in application_controller to find the customer
+    # if it find customer return it otherwise return error
+    def get_current_customer
+        if logged_in?
+            render json: current_customer
+        else
+            render json: {
+                error: "No one logged in"
+            }
+        end
+    end
     
 end
