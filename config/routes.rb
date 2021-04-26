b845f21bbfcbb9fb4d9d53754b1deff70894e3dc
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-26 07:30:43 -0400

    add route for logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post "api/v1/login", to: "api/v1/sessions#create"
+  delete "api/v1/logout", to: "api/v1/sessions#destroy"
   get "api/v1/get_current_customer", to: "api/v1/sessions#get_current_customer"
 
   namespace :api do
