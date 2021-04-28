0d27f883d5bf911fbb4e14b62098fcfdf28f5021
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-04-28 06:00:44 -0400

    add route for signup customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   post "api/v1/login", to: "api/v1/sessions#create"
+  post "api/v1/signup", to: "api/v1/customers#create"
   delete "api/v1/logout", to: "api/v1/sessions#destroy"
   get "api/v1/get_current_customer", to: "api/v1/sessions#get_current_customer"
 
   namespace :api do
     namespace :v1 do
       
-      resources :customers
+      resources :customers, only: [:show]
 
       resources :orders do
         resources :orderdetails
@@ -19,3 +20,5 @@ Rails.application.routes.draw do
     end
   end
 end
+
+
